25b1ca8a0bf9c28ccdf40b1d2665dc4ef704164f
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-05-26 23:42:00 +0500

    Create lists.dart

---
+++ flapshopadmin/lib/const/lists.dart
@@ -0,0 +1,9 @@
+import 'package:flapshopadmin/const/const.dart';
+
+const profileIconsList = [icshopsettings, icchat, icaccount];
+const meunuPopupTitle = [featured, edit, remove];
+const profileIconsTitle = [shopSettings, message, logout];
+const meunuPopupIcons = [Icons.featured_play_list, Icons.edit, Icons.delete];
+
+const profileButtonTitle = [shopSettings, message];
+const profileButtonIcons = [Icons.settings, Icons.chat];
