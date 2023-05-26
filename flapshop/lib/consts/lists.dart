0448025f22f2525e2860f368c1cc4088e6971333
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-05-26 23:32:12 +0500

    Create lists.dart

---
+++ flapshop/lib/consts/lists.dart
@@ -0,0 +1,56 @@
+import "package:flapshop/consts/consts.dart";
+
+const socialIconList = [
+  icFacebookLogo,
+  icTwitterLogo,
+  icGoogleLogo,
+];
+const sliderslist = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
+const secondsliderslist = [imgSs1, imgSs2, imgSs3, imgSs4];
+const featuredimages1 = [imgS1, imgS2, imgS3];
+const featuredimages2 = [imgS4, imgS5, imgS6];
+
+const featuredtitle1 = [laptop, ram, ssd];
+const featuredtitle2 = [harddisk, processor, keyboard];
+
+const categoriesList = [
+  laptop,
+  ram,
+  ssd,
+  harddisk,
+  processor,
+  keyboard,
+  cases,
+  gpu,
+  mouse,
+];
+const categoriesImgList = [
+  imgFc1,
+  imgFc2,
+  imgFc3,
+  imgFc4,
+  imgFc5,
+  imgFc6,
+  imgFc7,
+  imgFc8,
+  imgFc9
+];
+const itemDetailsButtonList = [
+  videos,
+  reviews,
+  sellerpolicy,
+  returnpolicy,
+  supportpolicy
+];
+const profilebuttonlist = [
+  order,
+  wishList,
+  messages,
+];
+const profilebuttoniconlist = [
+  icOrder,
+  icOrder,
+  icMessages,
+];
+const paymentMethodImg = [imgPaypal, imgStripe, imgCod];
+const paymentMethod = [paypal, stripe, cod];
