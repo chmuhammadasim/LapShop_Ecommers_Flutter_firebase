0a4b978cb5938ac94bb5c47c90d3d86b833fa218
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-05-26 23:34:24 +0500

    Create bg_widget.dart

---
+++ flapshop/lib/widgets_common/bg_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flapshop/consts/consts.dart';
+import 'package:flutter/material.dart';
+
+Widget bgWidget({Widget? child}) {
+  return Container(
+    decoration: const BoxDecoration(
+      image:
+          DecorationImage(image: AssetImage(imgBackground), fit: BoxFit.fill),
+    ),
+    child: child,
+  );
+}
