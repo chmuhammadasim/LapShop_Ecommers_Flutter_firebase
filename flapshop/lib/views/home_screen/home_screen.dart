703d53755c81c888d95812e35bb1a1ec09084b09
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-05-26 23:34:03 +0500

    Create home_screen.dart

---
+++ flapshop/lib/views/home_screen/home_screen.dart
@@ -0,0 +1,354 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flapshop/consts/consts.dart';
+import 'package:flapshop/consts/lists.dart';
+import 'package:flapshop/controllers/home_controller.dart';
+import 'package:flapshop/services/firestore_services.dart';
+import 'package:flapshop/views/category_screen/item_details.dart';
+import 'package:flapshop/views/home_screen/components/feature_button.dart';
+import 'package:flapshop/views/home_screen/components/search_screen.dart';
+import 'package:flapshop/widgets_common/home_buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var controller = Get.find<HomeController>();
+    return Container(
+      padding: const EdgeInsets.all(12),
+      color: lightGrey,
+      width: context.screenWidth,
+      height: context.screenHeight,
+      child: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 60,
+              color: lightGrey,
+              child: TextFormField(
+                controller: controller.searchController,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    suffixIcon: const Icon(Icons.search).onTap(() {
+                      if (controller
+                          .searchController.text.isNotEmptyAndNotNull) {
+                        Get.to(
+                          () => SearchScreen(
+                            title: controller.searchController.text,
+                          ),
+                        );
+                      }
+                    }),
+                    filled: true,
+                    fillColor: whiteColor,
+                    hintText: searchanything,
+                    hintStyle: const TextStyle(color: textfieldGrey)),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      height: 120,
+                      enlargeCenterPage: true,
+                      itemCount: sliderslist.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          sliderslist[index],
+                          fit: BoxFit.fitWidth,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(const EdgeInsets.symmetric(horizontal: 8))
+                            .make();
+                      },
+                    ),
+                    10.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                        2,
+                        (index) => homebutton(
+                          height: context.screenHeight * 0.15,
+                          width: context.screenWidth / 2.5,
+                          icon: index == 0 ? icTodaysDeal : icFlashDeal,
+                          title: index == 0 ? todaydeal : flashdeal,
+                        ),
+                      ),
+                    ),
+                    10.heightBox,
+                    VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      height: 120,
+                      enlargeCenterPage: true,
+                      itemCount: secondsliderslist.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          secondsliderslist[index],
+                          fit: BoxFit.fitWidth,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(const EdgeInsets.symmetric(horizontal: 8))
+                            .make();
+                      },
+                    ),
+                    10.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                        3,
+                        (index) => homebutton(
+                          height: context.screenHeight * 0.15,
+                          width: context.screenWidth / 3.5,
+                          icon: index == 0
+                              ? icTopCategories
+                              : index == 1
+                                  ? icBrands
+                                  : icTopSeller,
+                          title: index == 0
+                              ? topCategories
+                              : index == 1
+                                  ? brand
+                                  : topsellers,
+                        ),
+                      ),
+                    ),
+                    20.heightBox,
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: featuredCategories.text
+                          .color(darkFontGrey)
+                          .size(18)
+                          .fontFamily(semibold)
+                          .make(),
+                    ),
+                    20.heightBox,
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List.generate(
+                          3,
+                          (index) => Column(
+                            children: [
+                              featuredButton(
+                                icon: featuredimages1[index],
+                                title: featuredtitle1[index],
+                              ),
+                              10.heightBox,
+                              featuredButton(
+                                icon: featuredimages2[index],
+                                title: featuredtitle2[index],
+                              ),
+                            ],
+                          ),
+                        ).toList(),
+                      ),
+                    ),
+                    20.heightBox,
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        color: redColor,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          featuredProducts.text
+                              .color(whiteColor)
+                              .fontFamily(bold)
+                              .size(18)
+                              .make(),
+                          10.heightBox,
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: FutureBuilder(
+                              future: FireStoreService.featuredProducts(),
+                              builder: (BuildContext context,
+                                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                                ////////////////////////////////
+                                if (!snapshot.hasData) {
+                                  return const Center(
+                                    child: CircularProgressIndicator(
+                                      valueColor:
+                                          AlwaysStoppedAnimation(redColor),
+                                    ),
+                                  );
+                                } else if (snapshot.data!.docs.isEmpty) {
+                                  return "No Featured Products"
+                                      .text
+                                      .white
+                                      .makeCentered();
+                                } else {
+                                  var featuredData = snapshot.data!.docs;
+                                  return Row(
+                                    children: List.generate(
+                                      featuredData.length,
+                                      (index) => Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Image.network(
+                                            featuredData[index]['p_images'][0],
+                                            width: 150,
+                                            height: 150,
+                                            fit: BoxFit.cover,
+                                          ),
+                                          10.heightBox,
+                                          "${featuredData[index]['p_name']}"
+                                              .text
+                                              .fontFamily(semibold)
+                                              .color(darkFontGrey)
+                                              .make(),
+                                          10.heightBox,
+                                          "${featuredData[index]['p_price']}"
+                                              .numCurrency
+                                              .text
+                                              .color(redColor)
+                                              .fontFamily(bold)
+                                              .size(16)
+                                              .make(),
+                                        ],
+                                      )
+                                          .onTap(() {
+                                            Get.to(
+                                              () => ItemDetail(
+                                                title: featuredData[index]
+                                                    ['p_name'],
+                                                data: featuredData[index],
+                                              ),
+                                            );
+                                          })
+                                          .box
+                                          .white
+                                          .margin(
+                                            const EdgeInsets.symmetric(
+                                              horizontal: 4,
+                                            ),
+                                          )
+                                          .roundedSM
+                                          .padding(
+                                            const EdgeInsets.all(8),
+                                          )
+                                          .make(),
+                                    ),
+                                  );
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    20.heightBox,
+                    VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      height: 120,
+                      enlargeCenterPage: true,
+                      itemCount: secondsliderslist.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          secondsliderslist[index],
+                          fit: BoxFit.fitWidth,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(const EdgeInsets.symmetric(horizontal: 8))
+                            .make();
+                      },
+                    ),
+                    20.heightBox,
+                    StreamBuilder(
+                      stream: FireStoreService.allProducts(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation(redColor),
+                            ),
+                          );
+                        } else {
+                          var allproductdata = snapshot.data!.docs;
+                          return GridView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: allproductdata.length,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              mainAxisSpacing: 8,
+                              crossAxisSpacing: 8,
+                              mainAxisExtent: 300,
+                            ),
+                            itemBuilder: (context, index) {
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.network(
+                                    allproductdata[index]['p_images'][0],
+                                    width: 200,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  const Spacer(),
+                                  "${allproductdata[index]['p_name']}"
+                                      .text
+                                      .fontFamily(semibold)
+                                      .color(darkFontGrey)
+                                      .make(),
+                                  10.heightBox,
+                                  "${allproductdata[index]['p_price']}"
+                                      .numCurrency
+                                      .text
+                                      .color(redColor)
+                                      .fontFamily(bold)
+                                      .size(16)
+                                      .make(),
+                                ],
+                              )
+                                  .box
+                                  .white
+                                  .margin(
+                                      const EdgeInsets.symmetric(horizontal: 4))
+                                  .roundedSM
+                                  .padding(const EdgeInsets.all(12))
+                                  .make()
+                                  .onTap(
+                                () {
+                                  Get.to(
+                                    () => ItemDetail(
+                                      title: allproductdata[index]['p_name'],
+                                      data: allproductdata[index],
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                          );
+                        }
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
